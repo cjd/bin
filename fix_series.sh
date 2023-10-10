78e8bb629974f4315440f12946eca81d2ed10f69
Chris Debenham <chris@adebenham.com>
2023-10-10 20:17:04 +1100

    Make fix_series work _within_ the container

--- fix_series.sh
+++ fix_series.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# shellcheck disable=2034
-kubectl scale --replicas=0 statefulset/komga
-kubectl wait --for=delete pod/komga-0
-sleep 5
-SQLITE="sqlite3 /k8s/default/komga-config/database.sqlite"
-$SQLITE "select series_id,url,title from series_metadata, series where series.id=series_metadata.series_id;" | while read -r SERIES
+# shellcheck disable=2086
+KE="kubectl exec -itq pod/komga-0 -n default -c komga -- "
+$KE apt update
+$KE apt install sqlite3
+SQLITE="sqlite3 /config/database.sqlite"
+$KE $SQLITE "select series_id,url,title from series_metadata, series where series.id=series_metadata.series_id;" | while read -r SERIES
 do SERIES_ID=$(echo "$SERIES" | cut -f1 -d"|")
-URL=$(echo "$SERIES" | cut -f2 -d"|" | sed -e 's/^.*202/202/g' -e 's/%20/ /g' -e 's/\/$//g')
-NAME=$(echo "$SERIES" | cut -f3 -d"|")
-$SQLITE "UPDATE series_metadata SET title_lock=1, title='${URL}', title_sort='${URL}' WHERE series_id='${SERIES_ID}'"
+NAME=$(echo "$SERIES" | cut -f2 -d"|" | sed -e 's/^.*202/202/g' -e 's/%20/ /g' -e 's/\/$//g')
+$KE $SQLITE "UPDATE series_metadata SET title_lock=1, title='${NAME}', title_sort='${NAME}' WHERE series_id='${SERIES_ID}'"
 done
-kubectl scale --replicas=1 statefulset/komga
