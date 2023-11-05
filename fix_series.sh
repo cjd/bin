11aa8e500cb2740b720e96c659971373fd848990
Chris Debenham <chris@adebenham.com>
2023-11-05 15:08:35 +1100

    Fix komga series without restarting komga

--- fix_series.sh
+++ fix_series.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # shellcheck disable=2086
 KE="kubectl exec -itq pod/komga-0 -n default -c komga -- "
-$KE apt update
-$KE apt install sqlite3
+if [ "$1" != "skip" ]; then
+	$KE apt update
+	$KE apt install sqlite3
+fi
 SQLITE="sqlite3 /config/database.sqlite"
-$KE $SQLITE "select series_id,url,title from series_metadata, series where series.id=series_metadata.series_id;" | while read -r SERIES
-do SERIES_ID=$(echo "$SERIES" | cut -f1 -d"|")
-NAME=$(echo "$SERIES" | cut -f2 -d"|" | sed -e 's/^.*202/202/g' -e 's/%20/ /g' -e 's/\/$//g')
-$KE $SQLITE "UPDATE series_metadata SET title_lock=1, title='${NAME}', title_sort='${NAME}' WHERE series_id='${SERIES_ID}'"
+rm /tmp/fix_comic.sh 2>/dev/null
+$KE $SQLITE "select series_id,url,title from series_metadata, series where series.id=series_metadata.series_id;" | while read -r SERIES; do
+	echo $SERIES
+	SERIES_ID=$(echo "$SERIES" | cut -f1 -d"|")
+	NAME=$(echo "$SERIES" | cut -f2 -d"|" | sed -e 's/^.*202/202/g' -e 's/%20/ /g' -e 's/\/$//g')
+	CMD="UPDATE series_metadata SET title_lock=1, title='${NAME}', title_sort='${NAME}' WHERE series_id='${SERIES_ID}'"
+	echo $KE $SQLITE \"$CMD\" >>/tmp/fix_comic.sh
 done
+sh /tmp/fix_comic.sh
