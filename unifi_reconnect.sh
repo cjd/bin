e1eb4afb9f0cdac02099464eb5a60156bf41e175
Chris Debenham <chris@adebenham.com>
2023-11-05 15:07:33 +1100

    Handle change from configmaps

--- unifi_reconnect.sh
+++ unifi_reconnect.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ~/git/UniFi-API-client || exit
-grep dhcp-host ~/Sync/Config/k8s/services/infra/dnsmasq.yml | cut -f2 -d, | while read -r I; do
+#grep dhcp-host ~/Sync/Config/k8s/services/infra/dnsmasq.yml | cut -f2 -d, | while read -r I; do
+cut -f3 -d" " /k8s/default/dnsmasq/dhcp.leases | while read -r I; do
 	if ! ping -c 1 -W 1 "$I" >/dev/null; then
-		M=$(grep dhcp-host ~/Sync/Config/k8s/services/infra/dnsmasq.yml | grep "$I" | cut -f1 -d, | cut -f2 -d=)
+		#M=$(grep dhcp-host ~/Sync/Config/k8s/services/infra/dnsmasq.yml | grep "$I" | cut -f1 -d, | cut -f2 -d=)
+		M=$(grep "$I" /k8s/default/dnsmasq/dhcp.leases | cut -f2 -d" ")
 		echo "$I" "$M"
 		php ./reconnect.php "$M"
 		echo
